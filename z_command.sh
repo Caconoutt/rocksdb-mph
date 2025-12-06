006eb6cf16a86854870e87a7d1071a2e540d86bb
xuexiaoying <camilla.xue@mail.utoronto.ca>
2025-12-06 17:43:31 -0500

    command update

--- z_command.sh
+++ z_command.sh
@@ -13,6 +13,8 @@ make -j2 data_block_perfect_mph_index_test # compile
 # === compile db_bench === #
 make clean
 make -j2 db_bench DEBUG_LEVEL=0 # compile not in debug mode
+make -j db_bench DEBUG_LEVEL=0 # compile not in debug mode
+make -j db_bench DEBUG_LEVEL=0 OPT="-O3 -DNDEBUG -march=native"
 
 # === perf profile ===
 make -j db_bench DEBUG_LEVEL=1 OPT_LEVEL=0 # build with debug
